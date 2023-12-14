0f25c4c32799f1f8da2f92933cdcde9644e82ff3
Tomasz Kleszczewski <tomekkleszczewski@gmail.com>
2023-12-14 15:21:33 +0100

    Add first version of home-section

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Image {
+  src: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-home',
   styleUrl: 'home.component.scss',
   templateUrl: 'home.component.html',
   standalone: true,
+  imports: [CommonModule],
 })
-class HomeComponent {}
+class HomeComponent {
+  images: Image[] = [
+    {
+      src: '../../../assets/user-1.png',
+      alt: 'user-1',
+    },
+    {
+      src: '../../../assets/user-2.png',
+      alt: 'user-2',
+    },
+    {
+      src: '../../../assets/user-3.png',
+      alt: 'user-3',
+    },
+  ];
+}
 
 export { HomeComponent };
